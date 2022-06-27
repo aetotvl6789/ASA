5f3adafffa644d7bc9880486a32476c00d0ceb71
aetotvl6789 <nguyenviethuanlove043@gmail.com>
2022-06-28 02:53:35 +0700

    Delete MobileAppSecurityAssessment directory

--- MobileAppSecurityAssessment/GenerateDoc.sh
+++
@@ -1,2 +0,0 @@
-pandoc -s -f gfm --reference-doc=../Images/MobileReference.docx --columns 10000 -t docx -o "ADA Mobile Application Security Assessment.docx" TitlePage.md README.md MobileSecurityGuide.md UsefulLinks.md
-
